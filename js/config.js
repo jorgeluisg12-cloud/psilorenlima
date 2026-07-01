b3d97e90a2e9be7b3fa72473c96b72ee3beaa022
jorgeluisg12-cloud <jorgeluisg12@gmail.com>
2026-06-30 21:25:43 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -41,7 +41,7 @@ const DATA_CONFIG = {
             {
                 icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-md" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" /></svg>`,
                 title: "Ansiedade",
-                description: "Preocupação excessiva, pensamentos acelerados, tensão constante. Trabalhamos juntas para entender de onde isso vem e criar formas de lidar com mais calma."
+                description: "Preocupação excessiva, pensamentos acelerados, tensão constante. Trabalhamos juntos para entender de onde isso vem e criar formas de lidar com mais calma."
             },
             {
                 icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-md" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" /></svg>`,
